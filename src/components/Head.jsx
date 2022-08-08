605a7702170be85cf128fefafe6381263fa81055
shalnsingh@outlook.com <shalnsingh@outlook.com>
2022-08-08 16:59:12 +0530

    updated head component

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 
-import {useState} from "react";
+import {useState,useEffect} from "react";
+
+import { useSelector, useDispatch } from "react-redux";
+import {updateStatus} from '../slice/navigationSlice';
 
 import styles from '../styles/head.styles.module.css';
 
-const Head = () => {
-//     useEffect=(()=>{
-// console.log("UseEffect Called...!")
-//     },[])
+const Head = (props) => 
+{const dispatch = useDispatch()
+var siteInfo = useSelector(state=> state.info);
+    useEffect=(()=>{
+console.log("UseEffect Called...!")
+    },[siteInfo])
+
 
     const [isvalid, setIsValid]=useState(true)
     const handleColor = ()=>{
@@ -15,12 +21,104 @@ const Head = () => {
         setIsValid(!isvalid)
         console.log(isvalid)
     }
+    console.log(siteInfo)
+    const handleOne = ()=>{
+        console.log("One Clicked");
+        props.showWelcome(true)
+        props.showHome(false)
+        props.showPlaning(false)
+        props.showFinal(false)
+
+        dispatch(updateStatus({
+            "welcome": {
+              "welcomeActive":true,
+              "welcomeVisited":false
+         
+          },
+          "home":{
+            "homeActive":false,
+            "homeVisited":false
+          },
+          "planing":{
+            "planingActive":false,
+            "planingVisited":false
+        }
+      
+          }))
+    }
+    const handleTwo = ()=>{
+      console.log("One Clicked");
+      props.showWelcome(false)
+      props.showHome(true)
+      props.showPlaning(false)
+      props.showFinal(false)
+      dispatch(updateStatus({
+          "welcome": {
+            "welcomeActive":false,
+            "welcomeVisited":true
+        },
+        "home":{
+          "homeActive":true,
+          "homeVisited":false
+        },
+        "planing":{
+          "planingActive":false,
+          "planingVisited":false
+      }
+    
+        }))
+  }
+  const handleThree = ()=>{
+    console.log("One Clicked");
+    props.showWelcome(false)
+    props.showHome(false)
+    props.showPlaning(true)
+    props.showFinal(false)
+    dispatch(updateStatus({
+        "welcome": {
+          "welcomeActive":false,
+          "welcomeVisited":true
+      },
+      "home":{
+        "homeActive":false,
+        "homeVisited":true
+      },
+      "planing":{
+        "planingActive":true,
+        "planingVisited":false
+    }
+  
+      }))
+}
+
+const handleFour = ()=>{
+  console.log("One Clicked");
+  props.showWelcome(false)
+  props.showHome(false)
+  props.showPlaning(false)
+  props.showFinal(true)
+  dispatch(updateStatus({
+      "welcome": {
+        "welcomeActive":false,
+        "welcomeVisited":true
+    },
+    "home":{
+      "homeActive":false,
+      "homeVisited":true
+    },
+    "planing":{
+      "planingActive":false,
+      "planingVisited":true
+  }
+
+    }))
+}
   return (
     <>
     <div className={styles.main}>
       <div className={styles.topBar}>
        Eden
-       <button onClick={handleColor}>Change</button>
+      
       </div >
       <div className={styles.wrapper}>
         <div className={styles.container}>
@@ -31,32 +129,33 @@ const Head = () => {
 </div> */}
            <div className={styles.one}>
            {/* <div className={styles.roundActive}> */}
-           <div className={`${isvalid ===true ? styles.roundActive : styles.roundVisited}`}>
+           <div onClick={handleOne} className={`${siteInfo.siteInfo.info.welcome.welcomeActive===true? styles.roundActive:siteInfo.siteInfo.info.welcome.welcomeVisited===true? styles.roundVisited: styles.roundActive 
+          }`}>
 
             <div className={styles.roundInner}>1</div>
            </div>
-           <div className={`${isvalid ===true ? styles.lineActive : styles.lineVisited}`}></div>
+           <div className={`${siteInfo.siteInfo.info.welcome.welcomeActive===true ?styles.lineActive : siteInfo.siteInfo.info.welcome.welcomeVisited===true? styles.lineVisited :styles.line }`}></div>
 
 </div>
 <div className={styles.one}>
            {/* <div className={styles.roundActive}> */}
-           <div className={`${isvalid ===true? styles.roundActive : styles.roundVisited}`}>
+           <div onClick={handleTwo} className={`${siteInfo.siteInfo.info.home.homeVisited===true? styles.roundVisited : siteInfo.siteInfo.info.home.homeVisited===true? styles.roundActive:styles.roundActive }`}>
 
            <div className={styles.roundInner}>2</div>
            </div>
-           <div className={`${isvalid ===true ? styles.line : styles.lineVisited}`}></div>
+           <div className={`${siteInfo.siteInfo.info.home.homeActive ===true ? styles.lineActive :siteInfo.siteInfo.info.home.homeVisited===true? styles.lineVisited:styles.line}`}></div>
 
 </div>
 <div className={styles.one}>
            {/* <div className={styles.roundActive}> */}
-           <div className={`${isvalid ===true ? styles.roundActive : styles.roundVisited}`}>
+           <div onClick={handleThree} className={`${siteInfo.siteInfo.info.planing.planingVisited===true? styles.roundVisited : siteInfo.siteInfo.info.planing.planingVisited===true? styles.roundActive:styles.roundActive }`}>
            <div className={styles.roundInner}>3</div>
            </div>
-           <div className={`${isvalid ===true ? styles.line : styles.lineVisited}`}></div>
+           <div  className={`${siteInfo.siteInfo.info.planing.planingActive ===true ? styles.lineActive :siteInfo.siteInfo.info.planing.planingVisited===true? styles.lineVisited:styles.line}`}></div>
 
 </div>
 <div className={styles.one}>
-           <div className={styles.round4}>
+           <div onClick={handleFour}className={`${siteInfo.siteInfo.info.final.visited===true? styles.roundVisited4 : styles.round4}`}>
            <div className={styles.roundInner}>4</div>
            </div>
           
