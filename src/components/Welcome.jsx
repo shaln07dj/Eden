3769f2a6291fe15d58fa2ce67e20919fbed8f2e0
shalnsingh@outlook.com <shalnsingh@outlook.com>
2022-08-08 17:00:22 +0530

    updated welcome component

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
 
-import styles from '../styles/welcome.styles.module.css';
+import { useEffect,useState } from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import {updateStatus} from '../slice/navigationSlice';
+
+import styles from '../styles/welcome.styles.module.css'; 
 
 
-const Welcome = () => {
+const Welcome = (props) => {
+  
+      useEffect=(()=>{
+console.log("UseEffect Called...!")
+    },[])
+
+  const dispatch = useDispatch()
+  const siteInfo = useSelector(state=> state.info);
+  const [active, setActive] = useState(siteInfo.siteInfo.info.welcome.welcomeActive)
+  const [visited, setVisited] = useState(siteInfo.siteInfo.info.welcome.welcomeVisited)
+
+  
+  const handleClick =()=>{
+        console.log("Welcome i Got Clicked..!")
+        if(siteInfo.siteInfo.info.welcome.welcomeActive===true){
+          setActive(false);
+        }
+
+        if (siteInfo.siteInfo.info.welcome.welcomeVisited===false){
+          setVisited(true);
+        }
+        
+        
+        dispatch(updateStatus({
+          "welcome": {
+            "welcomeActive":false,
+            "welcomeVisited":true
+        },
+        "home":{
+          "homeActive":true,
+          "homeVisited":false
+        }
+    
+        }))
+       props.showWelcome(false)
+        props.showHome(true)
+        // console.log(siteInfo.siteInfo.info.welcome.welcomeActive)
+
+  }
   return (
     <>
     <div  className={styles.container}>
@@ -19,8 +61,27 @@ const Welcome = () => {
                </div>
 
             </div>
-            <div className={styles.bottomInnerContent}>
-asdfghjklkjhg
+            <div className={styles.bottomInnerContainer}>
+              <div className={styles.bottomInnerContent}>  
+              <div className={styles.inputContainerOne}>
+              Welcome Full Name
+    
+              <input className={styles.inputs} type="" id="html" name="fav_language" />
+
+                
+                </div>
+                <div className={styles.inputContainerTwo}>
+              Display Name
+    
+              <input className={styles.inputs} type="" id="html" name="fav_language" />
+
+                
+                </div>
+                <div className={styles.inputContainerOne}>
+                <button onClick={handleClick} className={styles.btnStyle}>Create Workspace</button>
+                </div>
+              </div>
+
             </div>
     </div>
     </>
