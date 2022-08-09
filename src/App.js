9d34bf9eb13dd3df3fd15594e6f59c6170269aff
shalnsingh@outlook.com <shalnsingh@outlook.com>
2022-08-10 05:14:26 +0530

    updated files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-import { useSelector, useDispatch } from "react-redux";
-import { updateStatus } from './slice/navigationSlice';
-
-import Container from './components/Container';
+import Container from "./components/Container";
 
 function App() {
-  const dispatch = useDispatch()
-  const siteInfo = useSelector(state => state.info);
-  // console.log(siteInfo.siteInfo.info.welcome.active)
-
-  const handleClick = () => {
-    console.log("Clicked")
-    dispatch(updateStatus({
-      "welcome": "active",
-      "home": "active",
-
-    }))
-  }
   return (
     <div className="App">
       <Container />
--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-import { configureStore } from '@reduxjs/toolkit';
-import { Provider } from 'react-redux';
-import infoSlice from './slice/navigationSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import infoSlice from "./slice/navigationSlice";
 const store = configureStore({
   reducer: {
     info: infoSlice,
+  },
+});
 
-  }
-})
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
